98c665a6bb62070a59dd670b81a62ba823537acd
jennahand <jennaraehand@gmail.com>
2018-05-12 13:33:18 -0700

    Started implementing connections list

--- app/src/main/java/group4/tcss450/uw/edu/tcss450project/utils/AbstractAdapter.java
+++
@@ -1,4 +0,0 @@
-package group4.tcss450.uw.edu.tcss450project.utils;
-
-public class AbstractAdapter {
-}
---
+++ app/src/main/java/group4/tcss450/uw/edu/tcss450project/utils/ConversationsAdapter.java
@@ -0,0 +1,58 @@
+package group4.tcss450.uw.edu.tcss450project.utils;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import group4.tcss450.uw.edu.tcss450project.R;
+import group4.tcss450.uw.edu.tcss450project.model.Connection;
+
+public class ConversationsAdapter extends RecyclerView.Adapter<ConversationsAdapter.ViewHolder> {
+
+    Connection[] mDataSet;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    public ConversationsAdapter(Connection[] connections) {
+        mDataSet = connections;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ConversationsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                            int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_connection, parent, false);
+
+        ConversationsAdapter.ViewHolder vh = new ConversationsAdapter.ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ConversationsAdapter.ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataSet[position].getUserName());
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+}
